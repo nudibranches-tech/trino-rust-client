1bbf23affa83b456cf95a5364bd20a20f6ce124c
Yannick Utard <yannickutard@gmail.com>
2025-09-24 19:47:32 +0200

    Remove unused SerializePairIterator

--- src/types/util.rs
+++ src/types/util.rs
@@ -25,29 +25,6 @@ where
     }
 }
 
-pub struct SerializePairIterator<K: Serialize, V: Serialize, I: Iterator<Item = (K, V)> + Clone> {
-    pub iter: I,
-    pub size: Option<usize>,
-}
-
-impl<K, V, I> Serialize for SerializePairIterator<K, V, I>
-where
-    K: Serialize,
-    V: Serialize,
-    I: Iterator<Item = (K, V)> + Clone,
-{
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut s = serializer.serialize_map(self.size)?;
-        for (k, v) in self.iter.clone() {
-            s.serialize_entry(&k, &v)?;
-        }
-        s.end()
-    }
-}
-
 pub struct SerializeVecMap<K: Serialize, V: Serialize> {
     pub iter: Vec<(K, V)>,
 }
